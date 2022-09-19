6e3138b1a30d87550a593d928a563d26e880aa28
Arust <90620456+Arust-INSYS@users.noreply.github.com>
2022-09-19 10:53:19 -0500

    cambio de paginas

--- action.js
+++ action.js
@@ -1,6 +1,14 @@
 //<button type="button" onclick="document.getElementById('demo').style.display='none'">Click Me!</button>
 function myFunction() {
-    document.getElementById("demo").style.display='none';
+    document.getElementById("demo").style.display='block';
+    document.getElementById("demo1").style.display='none';
     
   }
+
+function myFunction1() {
+  document.getElementById("demo").style.display='none';
+  document.getElementById("demo1").style.display='block';
+    
+  }
+  
 
