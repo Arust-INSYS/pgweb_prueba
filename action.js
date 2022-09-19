8b95c428f08be440471016e94037860a60994c20
Arust <90620456+Arust-INSYS@users.noreply.github.com>
2022-09-19 00:06:12 -0500

    integracion de javascript

---
+++ action.js
@@ -0,0 +1,6 @@
+//<button type="button" onclick="document.getElementById('demo').style.display='none'">Click Me!</button>
+function myFunction() {
+    document.getElementById("demo").style.display='none';
+    
+  }
+
